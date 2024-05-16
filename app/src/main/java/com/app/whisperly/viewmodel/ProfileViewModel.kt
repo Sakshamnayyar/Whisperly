e98cd3247da51448ba7c57170356ec0526523788
Saksham Nayyar <sakshamnayyar101@gmail.com>
2024-05-16 14:29:20 -0400

    Added apk of current progress.

--- app/src/main/java/com/app/whisperly/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/app/whisperly/viewmodel/ProfileViewModel.kt
@@ -13,12 +13,6 @@ import javax.inject.Inject
 class ProfileViewModel @Inject constructor(
     private val preference: MPreference ): ViewModel() {
 
-    val progressProPic = MutableLiveData(false)
-
-    val profileUpdateState = MutableLiveData<LoadState>()
-
-    val checkUserNameState = MutableLiveData<LoadState>()
-
     val name = MutableLiveData("")
 
     val profilePicUrl = MutableLiveData("")
@@ -26,10 +20,6 @@ class ProfileViewModel @Inject constructor(
     private var about = ""
 
     private var createdAt: Long = System.currentTimeMillis()
-
-//    private val storageRef = UserUtils.getStorageRef(context)
-//
-//    private val docuRef = UserUtils.getDocumentRef(context)
 
     init {
         val userProfile = preference.getUserProfile()
@@ -41,54 +31,4 @@ class ProfileViewModel @Inject constructor(
         }
     }
 
-
-//    fun uploadProfileImage(imagePath: Uri) {
-//        try {
-//            progressProPic.value = true
-//            val child = storageRef.child("profile_picture_${System.currentTimeMillis()}.jpg")
-//            val task = child.putFile(imagePath)
-//            task.addOnSuccessListener {
-//                child.downloadUrl.addOnCompleteListener { taskResult ->
-//                    progressProPic.value = false
-//                    profilePicUrl.value = taskResult.result.toString()
-//                }.addOnFailureListener {
-//                    OnFailureListener { e ->
-//                        progressProPic.value = false
-//                        context.toast(e.message.toString())
-//                    }
-//                }
-//            }.addOnProgressListener { taskSnapshot ->
-//                val progress: Double =
-//                    100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
-//            }
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//    }
-
-
-//    fun storeProfileData() {
-//        try {
-//            profileUpdateState.value = LoadState.OnLoading
-//            val profile = UserProfile(
-//                preference.getUid()!!,
-//                createdAt,
-//                System.currentTimeMillis(),
-//                profilePicUrl.value!!,
-//                name.value!!.toLowerCase(Locale.getDefault()),
-//                about,
-//                mobile = preference.getMobile(),
-//                token = preference.getPushToken().toString()
-//            )
-//            docuRef.set(profile, SetOptions.merge()).addOnSuccessListener {
-//                preference.saveProfile(profile)
-//                profileUpdateState.value = LoadState.OnSuccess()
-//            }.addOnFailureListener { e ->
-//                context.toast(e.message.toString())
-//                profileUpdateState.value = LoadState.OnFailure(e)
-//            }
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//    }
 }
